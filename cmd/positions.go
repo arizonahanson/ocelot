73c731b16ad348a37e5bf9d1e12f7b2de18efc7c
Arizona Hanson <arizona@starlig.ht>
2022-01-30 13:10:07 -0500

    show positions: update description

--- cmd/positions.go
+++ cmd/positions.go
@@ -31,15 +31,9 @@ import (
 // positionsCmd represents the positions command
 var positionsCmd = &cobra.Command{
 	Use:   "positions",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Display the currently held positions.",
+	Long:  `Display the currently held positions.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("positions called")
 		positions, err := Ocelot.GetPositions()
 		if err != nil {
 			log.Printf("error: %s", err)
@@ -50,14 +44,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	showCmd.AddCommand(positionsCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// positionsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// positionsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
